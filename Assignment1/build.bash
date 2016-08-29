acfa480d36aa9a726fad6c64b30bd4fbb78db77a
ablissar <ablissar@gmail.com>
2016-08-29 17:02:39 -0500

    Added rest of Assignment1 and Assignment2 files

---
+++ Assignment1/build.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -u -e
+echo ""
+echo ""
+echo ""
+echo "Building"
+javac Main.java
+echo ""
+echo ""
+echo ""
+java Main
---
+++ Assignment1/src/src/build.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -u -e
+echo ""
+echo ""
+echo ""
+echo "Building"
+javac Assignment1.java
+echo ""
+echo ""
+echo ""
+java Assignment1
