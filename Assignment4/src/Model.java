7d6bfc62a0e78bd7f4c8f68bf9906e4c73de08b3
Adam Bliss <ablissar@gmail.com>
2016-09-16 14:07:28 -0500

    Completed Step 3

--- Assignment4/src/Model.java
+++ Assignment4/src/Model.java
@@ -1,16 +1,30 @@
 import java.awt.Graphics;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 class Model
 {
-    private Sprite sprite;
+    private ArrayList<Sprite> sprites = new ArrayList<>();
 
-    Model() throws IOException {
-		sprite = new Sprite("smiley.jpg");
+    Model() throws IOException 
+    {
+		sprites.add( new Sprite("smiley.jpg") );
+		sprites.add( new Sprite("smiley.jpg") );
+		sprites.add( new Sprite("smiley.jpg") );
+		
+		sprites.get(1).setX(100);
+		sprites.get(1).setY(100);
+		
+		sprites.get(2).setX(200);
+		sprites.get(2).setY(200);
     }
 
-    public void update(Graphics g) {
-		sprite.update(g);
+    public void update(Graphics g) 
+    {
+		for( Sprite sprite : sprites)
+		{
+			sprite.update(g);
+		}
     }
 }
