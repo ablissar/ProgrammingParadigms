296f8ed8588e0ed4e34b9b192d6fbdff826b1506
Adam Bliss <ablissar@gmail.com>
2016-11-29 22:39:11 -0600

    Fixed misc issues w/ previous 2 commits

--- HonorsProject/src/Token.java
+++ HonorsProject/src/Token.java
@@ -31,19 +31,7 @@ public class Token extends Sprite {
 		case 3:
 			setY(getY()-1);
 		}
-		// Series of checks to keep player in bounds
-		if( getX() > width - 60 ) {
-			setX(width - 60);
-		}
-		if( getX() < 0 ) {
-			setX(0);
-		}
-		if( getY() > height - 60 ) {
-			setY(height - 60);
-		}
-		if( getY() < 0 ) {
-			setY(0);
-		}
+		inBounds(width, height);
 	}
 	
 	public void removed() {
