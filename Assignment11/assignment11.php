9742c39e5c558630fe7a3346268e2b2cfd73aaa4
Adam Bliss <ablissar@gmail.com>
2016-12-05 21:07:49 -0600

    Added both initial forms

---
+++ Assignment11/assignment11.php
@@ -0,0 +1,18 @@
+<html>
+    <title>Welcome to Adam Bliss's Assignment 11 PHP page!</title>
+    <h1>Welcome to Adam Bliss's Assignment 11 PHP page!</h1>
+    <body>
+        <p> Existing Users:
+        <form action="assignment11.php" method="get">
+            Username: <input type="text" name="username" /> <br />
+            Password: <input type="text" name="password" /> <br />
+            <input type="submit" /> <br />
+        </form>
+        <p> New Users:
+        <form action="assignment11.php" method="get">
+            Choose your username: <input type="text" name="newUsername" /> <br />
+            Choose your password: <input type="text" name="newPassword" /> <br />
+            <input type="submit" /> <br />
+        </form>
+    </body>
+</html>
