4fb53595ce0ba5da644edad3d341f7494c005eb2
Adam Bliss <ablissar@gmail.com>
2016-12-07 22:32:38 -0600

    Fixed issue w/ session persistence

--- Assignment11/assignment11.php
+++ Assignment11/assignment11.php
@@ -1,15 +1,14 @@
 <?php
+session_start();
 
 // Function that gets user info and stores it in session
 function getUserInfo($username, $password) {
-    session_start();
     $file = fopen("assignment11-account-info.txt", "r") or exit("Unable to open user file");
     while(!feof($file)) {
         $line = trim(fgets($file));
         if (strlen($line) > 0) {
             $arr = explode(";", $line);
             if($username == $arr[0] && $password == $arr[1]) {
-                echo "Test";
                 $_SESSION['firstName'] = $arr[2];
                 $_SESSION['lastName'] = $arr[3];
                 $_SESSION['color'] = $arr[4];
@@ -36,20 +35,25 @@ function validateUserInfo($username, $password) {
     return false;
 }
 
-// Page to be displayed if user inputs info that matches file
+// === Start of website code ===
+
+// If user enter info that matches the file, set the session info
 if( isset($_REQUEST['username']) && isset($_REQUEST['password'])
     && validateUserInfo($_REQUEST['username'], $_REQUEST['password']) ) {
-    getUserInfo($_REQUEST['username'], $_REQUEST['password']); ?>
+    getUserInfo($_REQUEST['username'], $_REQUEST['password']);
+}
+
+// Page to be displayed if session info is set
+if( isset($_SESSION['firstName']) ) { ?>
     <html>
     <body style="background-color: <?php echo $_SESSION['color']; ?>">
         <title> <?php echo $_SESSION['title']; ?> </title>
         <h1> <?php echo $_SESSION['title']; ?> </h1>
-        <img src="<?php echo $_SESSION['imageLink']; ?>" alt="User's Image">
+        <img src="<?php echo $_SESSION['imageLink']; ?>" alt="User's Image" style="width:15%">
     </body>
     </html>
-
 <?php
-}
+    }
 
 // Login form
 else { ?>
