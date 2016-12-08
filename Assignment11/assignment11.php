018ce54060a9307866e92726b747bc18b016b334
Adam Bliss <ablissar@gmail.com>
2016-12-07 18:07:54 -0600

    Added file input function

--- Assignment11/assignment11.php
+++ Assignment11/assignment11.php
@@ -1,3 +1,26 @@
+<?php
+    function getUserInfo() {
+        $file = fopen("assignment11-account-info.txt", "r") or exit("Unable to open user file");
+        $index = 0;
+        while(!feof($file)) {
+            $line = trim(fgets($file));
+            if (strlen($line) > 0) {
+                $arr = explode(";", $line);
+                $users[$index] = $arr[0];
+                $passwords[$index] = $arr[1];
+                $firstNames[$index] = $arr[2];
+                $lastNames[$index] = $arr[3];
+                $colors[$index] = $arr[4];
+                $titles[$index] = $arr[5];
+                $imageLinks[$index] = $arr[6];
+                $index = $index + 1;
+            }
+        }
+    }
+
+    getUserInfo();
+ ?>
+
 <html>
     <title>Welcome to Adam Bliss's Assignment 11 PHP page!</title>
     <h1>Welcome to Adam Bliss's Assignment 11 PHP page!</h1>
